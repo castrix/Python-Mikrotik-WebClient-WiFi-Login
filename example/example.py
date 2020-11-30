a586806e705559d305b3b7ef7765088c41dd472a
castrix <ihsanfajarramadhan5@gmail.com>
2020-11-30 23:10:27 +0700

    new version

--- example/example.py
+++ example/example.py
@@ -1,14 +1,15 @@
-import webclientlogin as w
+from python_mikrotik_login import mikrotikLogin
 
-username="your username"
-password="your password"
-minkey1=2625 # optional minimum index of the first unique encryption key
-maxkey1=2629 # optional maximum index of the first unique encryption key
-minkey2=2666 # optional minimum index of the second unique encryption key
-maxkey2=2730 # optional maximum index of the second unique encryption key
-url="http://loginpage.com/login" # Mikrotik login url
 
+username="ihsan"
+password="fajar"
+url="http://castrix.github.io/" # Mikrotik login url
+
+mikrotikLogin(username, password, url)
 # you can set the index manually or leave it empty
-# w.webClientLogin(username, password, url, minkey1, maxkey1, minkey2, maxkey2)
+# minkey1=2625 # optional minimum index of the first unique encryption key
+# maxkey1=2629 # optional maximum index of the first unique encryption key
+# minkey2=2666 # optional minimum index of the second unique encryption key
+# maxkey2=2730 # optional maximum index of the second unique encryption key
+# mikrotikLogin(username, password, url, minkey1, maxkey1, minkey2, maxkey2)
 
-w.webClientLogin(username, password, url)
