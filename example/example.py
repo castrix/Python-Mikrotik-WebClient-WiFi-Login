85b8307bc5b015cb59a77b4d42508a0df2a62d95
Ihsan Fajar Ramadhan <ihsanfajarramadhan5@gmail.com>
2020-11-30 20:29:35 +0700

    Update example.py

--- example/example.py
+++ example/example.py
@@ -2,10 +2,13 @@ import webclientlogin as w
 
 username="your username"
 password="your password"
-minkey1=2625 #minimum index of the first encryption key
-maxkey1=2629 #maximum index of the first encryption key
-minkey2=2666 #minimum index of the second encryption key
-maxkey2=2730 #maximum index of the second encryption key
-url="\login.html" #this should be url, the login.html file is just for you to test the index of the encryption key
+minkey1=2625 # optional minimum index of the first unique encryption key
+maxkey1=2629 # optional maximum index of the first unique encryption key
+minkey2=2666 # optional minimum index of the second unique encryption key
+maxkey2=2730 # optional maximum index of the second unique encryption key
+url="http://loginpage.com/login" # Mikrotik login url
 
-w.webClientLogin(username, password, minkey1, maxkey1, minkey2, maxkey2, url)
+# you can set the index manually or leave it empty
+# w.webClientLogin(username, password, url, minkey1, maxkey1, minkey2, maxkey2)
+
+w.webClientLogin(username, password, url)
