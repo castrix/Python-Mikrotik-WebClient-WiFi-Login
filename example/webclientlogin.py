67981afe40c45b1eea726f4f33f2b171c1b8f8e3
Ihsan Fajar Ramadhan <ihsanfajarramadhan5@gmail.com>
2020-11-30 20:19:05 +0700

    Update webclientlogin.py

--- example/webclientlogin.py
+++ example/webclientlogin.py
@@ -5,16 +5,26 @@ from md5 import md5
 #by ihsan fajar ramadhan
 #https://github.com/castrix
 
-def webClientLogin(username, password, minkey1, maxkey1, minkey2, maxkey2, url):
+def webClientLogin(username, password, url, minkey1=0, maxkey1=0, minkey2=0, maxkey2=0):
+    # with open('webclient.html', 'r') as file: #uncomment for simulation purpose
+    #     r= file.read()
     r= requests.get(url).text #requesting the url text
     key1=""
     key2=""
-    for i in range(minkey1, maxkey1): #6 characters, grabbing the first encription key, you should find the index yourself
-        key1 = key1+r[i]
-    for i in range(minkey2, maxkey2): #66 characters, grabbing the second encription key
-        key2 = key2+r[i]
+    if minkey1 == 0 or maxkey1 == 0 or minkey2 == 0 or maxkey2 == 0:
+        print("kosong")
+        a="document.sendin.password.value"
+        b=r.index(a)
+        key1=r[b+len(a)+11:b+len(a)+15]
+        key2=r[b+len(a)+52:b+len(a)+116]
+    else:
+        key1=r[minkey1:maxkey1]
+        key2=r[minkey2:maxkey2]
     key1=bytes(key1, "utf-8").decode("unicode_escape") #translating octal characters to char
     key2=bytes(key2, "utf-8").decode("unicode_escape") #translating octal characters to char
     encryptmd5= md5.hexMD5(key1+password+key2) #encrypt the password
-    finallogin=url+"username="+username+"&password="+encryptmd5 #wrap all the variables
-    requests.post(finallogin) #make the final requests
+    finallogin=url+"?username="+username+"&password="+encryptmd5 #wrap all the variables
+    final=requests.post(finallogin) #make the final requests
+    print(key1)
+    print(key2)
+    print(final)
