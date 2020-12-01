6bc3d18e7e7ca6b627cd872400cb3de3e7ee302a
Ihsan Fajar Ramadhan <ihsanfajarramadhan5@gmail.com>
2020-12-01 07:42:07 +0700

    Update example.py

--- example/example.py
+++ example/example.py
@@ -6,7 +6,7 @@ password="fajar"
 url="http://castrix.github.io/" # Mikrotik login url
 
 mikrotikLogin(username, password, url)
-# you can set the index manually or leave it empty
+# you can set the salt index manually (if the pattern is different) or leave it empty
 # minkey1=2625 # optional minimum index of the first unique encryption key
 # maxkey1=2629 # optional maximum index of the first unique encryption key
 # minkey2=2666 # optional minimum index of the second unique encryption key
