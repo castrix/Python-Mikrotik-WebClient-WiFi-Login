64f8c8e7c353bf304ad5cc33b42a4b8b40315499
castrix <ihsanfajarramadhan5@gmail.com>
2020-12-19 19:29:22 +0700

    fix conflicting dependencies

--- example/login_example.py
+++ example/login_example.py
@@ -1,11 +1,11 @@
 from python_mikrotik_login import MikrotikLogin
 
-username="admin"
-password="admin"
-url="http://10.x.x.x/login" #simulated Mikrotik login page url
-# username="ihsan"
-# password="fajar"
-# url="https://python-mikrotik-webclient-wifi-login.ihsanfr.repl.co/login" #You can test the package with this url
+# username="admin"
+# password="admin"
+# url="http://10.x.x.x/login" #simulated Mikrotik login page url
+username="ihsan"
+password="fajar"
+url="https://python-mikrotik-webclient-wifi-login.ihsanfr.repl.co/login" #You can test the package with this url
 
 login = MikrotikLogin(username, password, url)
 login.do_login()
